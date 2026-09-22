8f9aa1da747555e494f0db520e975a34c37c70f9
IjulienI <89165787+IjulienI@users.noreply.github.com>
2026-09-22 18:08:46 +0200

    chore(init) : add plugin skeleton

---
+++ Source/WireKitEditor/Private/WireKitEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "WireKitEditor.h"
+
+#define LOCTEXT_NAMESPACE "FWireKitEditorModule"
+
+void FWireKitEditorModule::StartupModule()
+{
+    
+}
+
+void FWireKitEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FWireKitEditorModule, WireKitEditor)
---
+++ Source/WireKitRuntime/Private/WireKitRuntime.cpp
@@ -0,0 +1,17 @@
+﻿#include "WireKitRuntime.h"
+
+#define LOCTEXT_NAMESPACE "FWireKitRuntimeModule"
+
+void FWireKitRuntimeModule::StartupModule()
+{
+    
+}
+
+void FWireKitRuntimeModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FWireKitRuntimeModule, WireKitRuntime)
