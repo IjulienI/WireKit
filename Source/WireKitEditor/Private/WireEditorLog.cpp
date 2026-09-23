2518d05a0a663834d761a8b80b887d70d5c7bcb4
julien <julienssmm@gmail.com>
2026-09-23 13:58:25 +0200

    feat(editor-log) : add LogWireKitEditor log category

--- Source/WireKitEditor/Private/WireEditorLog.cpp
+++ Source/WireKitEditor/Private/WireEditorLog.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 Julien soum. Licensed under the MIT License.
 
 
-#include "WireLog.h"
+#include "WireEditorLog.h"
 
-DEFINE_LOG_CATEGORY(LogWireKitRuntime);
+DEFINE_LOG_CATEGORY(LogWireKitEditor);
--- Source/WireKitEditor/Public/WireEditorLog.h
+++ Source/WireKitEditor/Public/WireEditorLog.h
@@ -4,4 +4,4 @@
 
 #include "CoreMinimal.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogWireKitRuntime, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogWireKitEditor, Log, All);
