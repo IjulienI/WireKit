1e266d93600482e2aaf1b4ad21b3f274fd5410ec
julien <julienssmm@gmail.com>
2026-09-23 11:14:52 +0200

    refactor(build-cs) : move dep to public and remove slate one

--- Source/WireKitRuntime/WireKitRuntime.Build.cs
+++ Source/WireKitRuntime/WireKitRuntime.Build.cs
@@ -10,16 +10,14 @@ public class WireKitRuntime : ModuleRules
             new string[]
             {
                 "Core",
+                "CoreUObject",
+                "Engine",
             }
         );
 
         PrivateDependencyModuleNames.AddRange(
             new string[]
             {
-                "CoreUObject",
-                "Engine",
-                "Slate",
-                "SlateCore"
             }
         );
     }
